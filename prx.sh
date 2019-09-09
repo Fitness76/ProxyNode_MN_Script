aa1baafc44816b3ef431889bd99cb18a53e2870d
Fitness76 <47913866+Fitness76@users.noreply.github.com>
2019-09-09 00:15:57 -0500

    Update prx.sh

--- prx.sh
+++ prx.sh
@@ -1,3 +1,4 @@
+  
 #!/bin/bash
 #script_Version:1.1
 #This script work was created by SBurns of the Null Entry Project
@@ -42,8 +43,8 @@ COINCORE=.proxynode
 COINCONFIG=prx.conf
 COINHOME=/home/proxynode
 #wallet downnload and extractions commands
-DOWNLOADCOINFILES=https://github.com/ProxyNode/proxynode/releases/download/v2.1.0/prx-Linux-v2.1.0.zip
-COINFILES=prx-Linux-v2.1.0.zip
+DOWNLOADCOINFILES=https://github.com/ProxyNode/proxynode/releases/download/v2.0.0/Proxynode-Linux-MN-v2.0.0.zip
+COINFILES=Proxynode-Linux-MN-v2.0.0.zip
 #need DECOMPRESS
 DECOMPRESS='unzip'
 #rocketstrap
