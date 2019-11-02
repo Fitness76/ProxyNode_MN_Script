78915205e36a6e9b983313e955508adaa888aeb9
Fitness76 <47913866+Fitness76@users.noreply.github.com>
2019-11-01 21:29:52 -0500

    Update prx2.sh

--- prx2.sh
+++ prx2.sh
@@ -1943,7 +1943,7 @@ Function_Build_Masternode_Key_Table(){
     echo -e ${RED}"This Will take a moment"${CLEAR}
     nodeunit=1
     Function_Start_Masternode
-    sleep 180
+    sleep 240
     sudo touch ${DPATH}${COIN3l}mnkey.tbl
     echo \#If editing IP Table list them below.  Starting from masternode 1 to 10 > ${DPATH}${COIN3l}mnkey.tbl
     echo \Masternode needs to be rebuilt in order for these to take effect >> ${DPATH}${COIN3l}mnkey.tbl
